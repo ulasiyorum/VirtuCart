6419d13a9e50c00d1d145ae8820231f6ca510852
Ulaş Gergerli <20200808049@ogr.akdeniz.edu.tr>
2023-12-17 19:58:22 +0300

    Initial commit

---
+++ src/test/java/com/ulasgergerli/virtucart/VirtuCart/VirtuCartApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ulasgergerli.virtucart.VirtuCart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VirtuCartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
